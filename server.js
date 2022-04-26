1cbf3ac22f7a5e480f4257eb77d6e52fee26d5b7
ertugocer <ertugrul09gocer@gmail.com>
2022-04-26 17:30:58 +0300

    .

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT)
 })
