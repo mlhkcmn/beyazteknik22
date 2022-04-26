710181b0654f9d7496af2b53f534a62ef4e3e2e6
ertugocer <ertugrul09gocer@gmail.com>
2022-04-26 17:06:50 +0300

    barcode

---
+++ client/src/components/common/Barcode.js
@@ -0,0 +1,20 @@
+import {useEffect, useRef} from 'react';
+
+function Barcode() {
+    const video = useRef(null);
+
+    useEffect(() => {
+        (effect)
+        return () => {
+            
+        }
+    }, [])
+
+    return (
+        <>
+            <video ref={video} autoPlay muted />
+        </>
+    );
+}
+
+export default Barcode;
