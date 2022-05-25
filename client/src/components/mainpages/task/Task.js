d4be7f8351a8da55d5660a52f6781aab9bd3d341
zcaymaz <zbyrcmyz@gmail.com>
2022-05-26 01:59:48 +0300

    bişiler yapıldı

---
+++ client/src/components/mainpages/task/Task.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TaskCard from './TaskCard'
+
+const task = () => {
+    return (
+        <>
+            <TaskCard/>
+        </>
+    )
+}
+
+export default task
